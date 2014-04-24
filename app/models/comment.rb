ceb56bf0c082e3843927445b13ee6a785bc9ce92
Aleksey <asharlaimov@gmail.com>
2014-04-24 22:20:51 +0300

    Update comments UI

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ActiveRecord::Base
-  validates :body, presence: true
   belongs_to :post
+  validates :body, presence: true
 end
--- db/migrate/20140424154009_create_comments.rb
+++ db/migrate/20140424154009_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
       t.text :body
-      t.belongs_to :blog, index: true
+      t.belongs_to :post, index: true
 
       t.timestamps
     end
