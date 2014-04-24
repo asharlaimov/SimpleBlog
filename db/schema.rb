701a85930efd3a6d34d421785ecbfd5c3f189850
Aleksey <asharlaimov@gmail.com>
2014-04-24 22:20:51 +0300

    Update comments UI

--- db/schema.rb
+++ db/schema.rb
@@ -15,12 +15,12 @@ ActiveRecord::Schema.define(version: 20140424154009) do
 
   create_table "comments", force: true do |t|
     t.text "body"
-    t.integer "blog_id"
+    t.integer "post_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "comments", ["blog_id"], name: "index_comments_on_blog_id"
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
 
   create_table "posts", force: true do |t|
     t.string "title"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Post.delete_all
+Comment.delete_all
+
+post1 = Post.create!(title: 'Post 1', body: 'Desc Post 1')
+post2 = Post.create!(title: 'Post 2', body: 'Desc Post 2')
+post3 = Post.create!(title: 'Post 3', body: 'Desc Post 3')
+
+Comment.create!(body: 'Comment 1', post: post1)
+Comment.create!(body: 'Comment 2', post: post2)
+Comment.create!(body: 'Comment 3', post: post3)
