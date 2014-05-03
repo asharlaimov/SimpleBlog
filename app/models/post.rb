1ec305373dd784a968c3c467bef44f95ab5753d9
Aleksey <asharlaimov@gmail.com>
2014-05-03 23:05:58 +0300

    fix delete post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   has_many :comments
   has_many :post_tags
-  has_many :tags, -> { distinct }, through: :post_tags
+  has_many :tags, -> { distinct }, through: :post_tags, :dependent => :destroy
   validates :title, :body, presence: true
 
   def init_tags(tags_list)
