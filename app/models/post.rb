f51ed5770b460787fdaf65415de22be178140e5a
Aleksey <asharlaimov@gmail.com>
2014-05-03 22:54:02 +0300

    paging and filtering posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,4 +10,14 @@ class Post < ActiveRecord::Base
       self.tags << Tag.find_or_initialize_by(:title => t)
     end
   end
+
+  def self.filter_by_tag(tag)
+    if tag.present?
+      includes(:tags).where("tags.id = ?", tag).references(:tags)
+    else
+      all
+    end
+  end
+
+  default_scope order('posts.created_at DESC')
 end
